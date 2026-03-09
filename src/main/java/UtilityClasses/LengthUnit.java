b96c537022aa59de26d66261565f6aaf7b4d2d5c
TomarAbhin <tomar.abhinav3107@gmail.com>
2026-03-09 11:10:37 +0530

    [Abhinav Tomar] Add. added UC8-StandaloneUnit

---
+++ src/main/java/UtilityClasses/LengthUnit.java
@@ -0,0 +1,29 @@
+package UtilityClasses;
+
+public enum LengthUnit {
+
+    FEET(1.0),
+    INCH(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
+
+    private final double conversionFactorToFeet;
+
+    LengthUnit(double conversionFactorToFeet) {
+        this.conversionFactorToFeet = conversionFactorToFeet;
+    }
+
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactorToFeet;
+    }
+
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactorToFeet;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
+    }
+}
